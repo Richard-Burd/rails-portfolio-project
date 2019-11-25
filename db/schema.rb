60a5b2dede9f250306a0b9acfbc97bbd9efdc9d3
Richard-Burd <qalbalassadalasfour@gmail.com>
2019-11-25 09:40:26 -0800

    objects collaborating in console

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_213844) do
+ActiveRecord::Schema.define(version: 2019_11_25_172844) do
+
+  create_table "legs", force: :cascade do |t|
+    t.string "description"
+    t.string "terrain"
+    t.integer "distance"
+    t.integer "altitude_change"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "trip_legs", force: :cascade do |t|
+    t.integer "trip_id"
+    t.integer "leg_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.string "name"
+    t.string "convoy_log"
+    t.string "start_location"
+    t.string "finish_location"
+    t.integer "total_fuel_actually_consumed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -18,6 +45,16 @@ ActiveRecord::Schema.define(version: 2019_11_05_213844) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "name"
+    t.integer "miles_per_gallon"
+    t.integer "fuel_on_vehicle"
+    t.integer "user_id"
+    t.integer "trip_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,15 @@
 User.create!(email: "ah@my.ai", username: "Ahmed", password: "pw1")
 User.create!(email: "beh@my.ai", username: "Behati", password: "pw2")
 User.create!(email: "car@my.ai", username: "Charles", password: "pw3")
+
+# Now I'm going to create a trip specifically to test the math; this trip will have nice round numbers
+Leg.create!(description: "First Test Leg", distance: 100, altitude_change: 0, terrain: "paved_road", user_id: 1)
+Leg.create!(description: "Second Test Leg", distance: 100, altitude_change: 0, terrain: "paved_road", user_id: 1)
+
+Trip.create!(name: "Numbers Tester", start_location: "Mile Zero", finish_location: "Mile One-Hundred", total_fuel_actually_consumed: nil)
+
+TripLeg.create!(trip_id: 1, leg_id: 1)
+TripLeg.create!(trip_id: 1, leg_id: 2)
+
+Vehicle.create!(name: "Trailer", miles_per_gallon: 20, fuel_on_vehicle: 10, user_id: 1, trip_id: 1)
+Vehicle.create!(name: "Ford SUV", miles_per_gallon: 20, fuel_on_vehicle: 10, user_id: 1, trip_id: 1)
