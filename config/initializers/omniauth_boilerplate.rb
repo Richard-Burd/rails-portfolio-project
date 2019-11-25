1ecc1f0ec1e058a80ad91e180b90d7f619a4ee51
Richard-Burd <qalbalassadalasfour@gmail.com>
2019-11-25 15:29:53 -0800

    blank views rendering

--- config/initializers/omniauth_boilerplate.rb
+++ config/initializers/omniauth_boilerplate.rb
@@ -1,24 +1,12 @@
-# https://www.youtube.com/watch?v=UAvuo-EbTFY (7:40 / 56:18)
-# OmniAuth Video
-
-# This Sinatra code was copied from here:
-# https://github.com/omniauth/omniauth-github
-# use OmniAuth::Builder do
-#   # provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-
-#   # NOTE; you cannot push this code to github because it will give people access
-#   # to your github account.
-#   # ENV['GITHUB_KEY'] = 0c................8f
-#   # ENV['GITHUB_SECRET'] = e4.........................................45
-#   provider :github, '0c................8f', 'e4.........................................45'
-
+# # NOTE: if you un-comment this file, the OmniAuth functionality will break.
+#
+# # OmniAuth Video in Flatiron curriculum is here:
+# # https://www.youtube.com/watch?v=UAvuo-EbTFY (7:40 / 56:18)
+#
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#                     # client key - identifies the GitHub application
+#   provider :github, 'the-client-id-key-goes-in-these-quotes-right-here',
+#
+#                     # client secret - the password for the GitHub application
+#                     'the-client-secret-key-goes-in-these-quotes-right-here'
 # end
-
-# This was also copied from watching the above video:
-Rails.application.config.middleware.use OmniAuth::Builder do
-                    # client key - identifies the GitHub application
-  provider :github, 'the-client-id-key-goes-in-these-quotes-right-here',
-
-                    # client secret - the password for the GitHub application
-                    'the-client-secret-key-goes-in-these-quotes-right-here'
-end
