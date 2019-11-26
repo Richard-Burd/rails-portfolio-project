f7ffd4c76fdc5c709d4132f28edfbbbb7993104b
Richard-Burd <qalbalassadalasfour@gmail.com>
2019-11-25 21:16:37 -0800

    index & show trips working

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,43 +1,54 @@
 class TripsController < ApplicationController
-  # this has the "Home" and "Logout" links at the bottom of each pags
-  # and is located here: app/views/layouts/burdlayout.html.erb
+  # After any user can edit or delete a Trip, we will set conditions so that
+  # a user can only edit or delete a trip in which they own a car on.
+  before_action :authentication_required #=> This is defined in the application controller because the application controller is an inheretance point
+
+  # this will specify to use the formatting in app/views/layouts/convoyapp.html.erb
   layout "burdlayout"
 
-  # the user must be "logged_in? = true" as defined in this inheretance point:
-  # app/controllers/application_controller.rb
-  before_action :authentication_required
-
   def index
-    # render :plain => "This is where you will see all the trips"
+    # render :plain => "You should only see this if you're logged in"
+    @trips = Trip.all
   end
 
   def show
-    # render :plain => "This is where you will see a trip"
-  end
-
-  def new
-    # render :plain => "This is where you will view a new trip form"
-  end
-
-  def create
-    # render :plain => "This is where you will create a trip"
+    # render :plain => "This should have a URL like so: localhost:3000/trips/1/show"
+    @trip = Trip.find(params[:id])
   end
 
   def edit
-    # render :plain => "This is where you will edit trips"
+    render :plain => "This is where you will edit a trip"
   end
 
   def update
-    render :plain => "This is where you will update trips"
+    # render :plain => "This is where you will save updates to edited trips"
+    @trip = Trip.find(params[:id])
+    @trip.update(trip_params(:name, :start_location, :finish_location, :total_fuel_actually_consumed, :convoy_log))
+#     @post = Post.new(title: params[:title], content: params[:content], author_id: params[:author])
+# @post = Post.new(post_params(:title, :content, :author)) <=# This isn't working properly yet
+    redirect_to trip_path(@trip)
   end
 
+  def new
+    render :plain => "This is where you will make a new trip"
+  end
+
+  def create
+    render :plain => "This is where a new trip will be saved to the database"
+  end
+
+  # https://learn.co/tracks/full-stack-web-development-v8/module-13-rails/section-6-validations-and-forms/delete-forms
   def destroy
-    render :plain => "This is where you will delete trips"
+    # render :plain => "This is where you will delete trips"
+    Trip.find(params[:id]).destroy
+    redirect_to trips_path
   end
 
   private
 
-  def vehicle_params(*args)
-    params.require(:vehicle).permit(*args)
+  # Strong Params
+  def trip_params(*args)
+    params.require(:trip).permit(*args)
   end
+
 end
--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -10,14 +10,28 @@ class VehiclesController < ApplicationController
 
   def index
     # render :plain => "This is where you will see all the vehicles"
+    # NOTE: no verification happens here, if you URL hack, you will be
+    # able to view other user's vehicles
+    @user = User.find(params[:user_id])
+    @vehicles = Vehicle.all.where(user_id: params[:user_id])
+    render template: 'vehicles/index'
   end
 
   def show
     # render :plain => "This is where you will see a vehicle"
+    # First we must verify that the user owns the vehicle they with to see.
+    if current_user.id == params[:user_id].to_i
+      @user = User.find(params[:user_id])
+      @vehicle = Vehicle.find(params[:id])
+    end
   end
 
   def new
-    # render :plain => "This is where you will view a new vehicle form"
+    # NOTE: no verification happens here, if you URL hack, you will be
+    # able to view a new vehicle form for another user!
+    @trips = Trip.all
+    @user = User.find(params[:user_id])
+    @vehicle = Vehicle.new
   end
 
   def create
