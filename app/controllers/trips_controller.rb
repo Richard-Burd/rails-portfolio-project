0bec96efe9e277538fc16ca0161bf3350190a4ee
Richard-Burd <qalbalassadalasfour@gmail.com>
2019-11-25 21:30:28 -0800

    edit & new vic working

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,7 +3,7 @@ class TripsController < ApplicationController
   # a user can only edit or delete a trip in which they own a car on.
   before_action :authentication_required #=> This is defined in the application controller because the application controller is an inheretance point
 
-  # this will specify to use the formatting in app/views/layouts/convoyapp.html.erb
+  # this will specify to use the formatting in app/views/layouts/burdlayout.html.erb
   layout "burdlayout"
 
   def index
@@ -17,16 +17,42 @@ class TripsController < ApplicationController
   end
 
   def edit
-    render :plain => "This is where you will edit a trip"
+    @legs = Leg.all
+    @vehicles = Vehicle.all
+    @trip = Trip.find(params[:id])
+    # This code fulfills one of the Rails Portfolio project requirements:
+    # You must include at least one class level ActiveRecord scope method.
+    # Your scope method must be chainable, meaning that you must use ActiveRecord
+    # Query methods within it (such as .where and .order) rather than native ruby
+    # methods (such as #find_all or #sort) <=# The code below uses "where"
+    if @trip.vehicles.where(user_id: current_user.id).exists? == true
+      # render :plain => "This is where you will edit a trip called: #{@trip.name}"
+    else
+      # render :plain => "You shouldn't have permission to edit this trip and you should never see this message!"
+      redirect_to trip_path(@trip)
+    end
   end
 
   def update
-    # render :plain => "This is where you will save updates to edited trips"
+    @vehicles = Vehicle.all
     @trip = Trip.find(params[:id])
-    @trip.update(trip_params(:name, :start_location, :finish_location, :total_fuel_actually_consumed, :convoy_log))
-#     @post = Post.new(title: params[:title], content: params[:content], author_id: params[:author])
-# @post = Post.new(post_params(:title, :content, :author)) <=# This isn't working properly yet
-    redirect_to trip_path(@trip)
+
+    @trip.update(trip_params)
+    # This no longer works because we are using Has Many Through in Forms now
+    # @trip.update(trip_params(:name, :start_location, :finish_location, :total_fuel_actually_consumed, :convoy_log, :vehicle_ids))
+
+    # This method isn't working so I cannot add vehicles directly to a trip
+    # @trip.update_column(trip_params(:vehicle_ids)) << params[:trip][:vehicle_ids]
+
+
+# @cust.house_ids << House.find(params[:house_id])
+
+    if @trip.valid?
+      @trip.save
+      redirect_to trip_path(@trip)
+    else
+      render :edit
+    end
   end
 
   def new
@@ -40,15 +66,25 @@ class TripsController < ApplicationController
   # https://learn.co/tracks/full-stack-web-development-v8/module-13-rails/section-6-validations-and-forms/delete-forms
   def destroy
     # render :plain => "This is where you will delete trips"
-    Trip.find(params[:id]).destroy
-    redirect_to trips_path
+    @trip = Trip.find(params[:id])
+    if @trip.vehicles.where(user_id: current_user.id).exists? == true
+      @trip.destroy
+      redirect_to trips_path
+    else
+      render :plain => "You shouldn't have permission to delete this trip and you should never see this message!"
+    end
   end
 
   private
 
   # Strong Params
-  def trip_params(*args)
-    params.require(:trip).permit(*args)
+  # In order to implement strong params, go to: config/application.rb and delete this line:
+  # config.action_controller.permit_all_parameters = true
+  # def trip_params(*args)
+  #   params.require(:trip).permit(*args)
+  # end
+  def trip_params
+    params.require(:trip).permit(:name, :start_location, :finish_location, :total_fuel_actually_consumed, :convoy_log, leg_ids:[], vehicle_ids:[])
   end
 
 end
--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,53 +1,76 @@
 require 'pry'
 class VehiclesController < ApplicationController
-  # this has the "Home" and "Logout" links at the bottom of each pags
-  # and is located here: app/views/layouts/burdlayout.html.erb
   layout "burdlayout"
-
-  # the user must be "logged_in? = true" as defined in this inheretance point:
-  # app/controllers/application_controller.rb
-  before_action :authentication_required
+  before_action :authentication_required #=> This is defined in the application controller because the application controller is an inheretance point
 
   def index
-    # render :plain => "This is where you will see all the vehicles"
-    # NOTE: no verification happens here, if you URL hack, you will be
-    # able to view other user's vehicles
+    # render :plain => "This is where a user's vehicles will be shown"
+
     @user = User.find(params[:user_id])
     @vehicles = Vehicle.all.where(user_id: params[:user_id])
     render template: 'vehicles/index'
+    # This is sorta copied from here:
+    # https://learn.co/tracks/full-stack-web-development-v8/module-13-rails/section-10-routes-and-resources/routing-and-nested-resources
+    # # app/controllers/posts_controller.rb
+    # but it's not working
+
+    #if params[:user_id]
+    #  @vehicles = User.find(params[:user_id]).vehicles
+    #else
+    #  @vehicles = Vehicle.all
+    #end
+
+    ############################ LEFT OFF HERE #################################
+    # @vehicles = Vehicle.all.where(user_id: current_user.id)
   end
 
   def show
-    # render :plain => "This is where you will see a vehicle"
-    # First we must verify that the user owns the vehicle they with to see.
-    if current_user.id == params[:user_id].to_i
-      @user = User.find(params[:user_id])
-      @vehicle = Vehicle.find(params[:id])
-    end
+    # render :plain => "This is where an individual vehicle will be shown"
+    @user = User.find(params[:user_id])
+    @vehicle = Vehicle.find(params[:id])
+    # binding.pry
   end
 
   def new
-    # NOTE: no verification happens here, if you URL hack, you will be
-    # able to view a new vehicle form for another user!
+    # render :plain => "This is where you will make a new vehicle"
     @trips = Trip.all
     @user = User.find(params[:user_id])
     @vehicle = Vehicle.new
   end
-
+  #@authors = Author.all
+  #@post = Post.new(post_params(:title, :content, :author_id))
   def create
-    # render :plain => "This is where you will create a vehicle"
+    @trips = Trip.all
+    # render :plain => "This is where a new vehicle will be saved to the database"
+    @user = User.find(params[:user_id])
+    @vehicle = Vehicle.new(vehicle_params(:name, :miles_per_gallon, :fuel_on_vehicle, :trip_id))
+    @vehicle.user_id = @user.id
+    @vehicle.trip_id = params[:trip_id]
+    # binding.pry
+    # raise params.inspect
+    if @vehicle.valid?
+      @vehicle.save
+      # redirect_to root_path
+      redirect_to user_vehicles_path
+    else
+      render :new
+    end
   end
 
   def edit
-    # render :plain => "This is where you will edit vehicles"
-  end
-
-  def update
-    render :plain => "This is where you will update vehicles"
+    render :plain => "This is where an existing vehicle will be edited"
+    # binding.pry
   end
 
   def destroy
-    render :plain => "This is where you will delete vehicles"
+    # render :plain => "This is where you will delete trips"
+    @vehicle = Vehicle.find(params[:id])
+    if @vehicle.user_id == current_user.id
+      @vehicle.destroy
+      redirect_to user_vehicles_path
+    else
+      render :plain => "You shouldn't have permission to delete this trip and you should never see this message!"
+    end
   end
 
   private
